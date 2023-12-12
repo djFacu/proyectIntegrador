387ecd121d0f5c297098dc892c499d9cc52711c3
djFacu <facundo.coronel@bue.edu.ar>
2023-12-12 15:25:23 -0300

    mensaje del commit

---
+++ member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Ejemplo1","l":"Ejemplo1()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ src/PrimerosNumeros2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class PrimerosNumeros2 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		System.out.println("Ingresun valor");
+		int i = 1;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= n);
+	}
+}
